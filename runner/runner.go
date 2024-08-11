3baa43b50a9e1ded3ca8e4a1a9660973af07e93d
daijinru <jeocat@163.com>
2024-08-11 15:50:31 +0800

    :bug: unlock before send callback

--- runner/runner.go
+++ runner/runner.go
@@ -82,6 +82,8 @@ func (runner *Runner) Create() error {
 		runner.Logger.Warn(err.Error())
 		return err
 	}
+	// Temporary fix, if it is not unlock, the next task will not be executed after send Callback below.
+	runner.Complete()
 
 	if runner.Callback != "" {
 		err = runner.Pipeline.Callback(
